060ef529c5427471189a3b9db30659a6a40a4c0b
Mostafa-YMS <mostafa_yms@yahoo.com>
2021-12-31 21:47:29 +0200

    clean

--- ubus/urls.py
+++ ubus/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from linesStops.views import Linesview , stationsview , Cordsview
 from django.views.generic import TemplateView
 
 urlpatterns = [
